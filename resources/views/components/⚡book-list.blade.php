91a8b4291199d5fe849696852ad279a97e6c4a5a
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-17 08:32:53 +0430

    new files and changes

--- resources/views/components/⚡book-list.blade.php
+++ resources/views/components/⚡book-list.blade.php
@@ -5,9 +5,12 @@ use Livewire\Component;
 new class extends Component
 {
     //
+    public $myName="Mahtab";
 };
 ?>
 
 <div>
+    <h1>Hi I am {{$myName}}</h1>
+    <h1>Hi there i'm coming from booklist component</h1>
     {{-- No surplus words or unnecessary actions. - Marcus Aurelius --}}
 </div>
