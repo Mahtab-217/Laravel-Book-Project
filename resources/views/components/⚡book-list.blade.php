576e0b9e42606c6c8284867c5b617c3cd004ae1c
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-18 21:56:09 +0430

    new

--- resources/views/components/⚡book-list.blade.php
+++ resources/views/components/⚡book-list.blade.php
@@ -16,7 +16,7 @@ new class extends Component
 
    public function deleteBook($id){
     $this->bookId= $id;
-    $this->isModelOpen=true;
+    $this->isModalOpen=true;
 
    }
 
@@ -25,7 +25,7 @@ function cancel(){
     $this->isModalOpen=false;
    }
    public function deleteBtn(){
-    Book::findOrFail($this->bookId);
+    $book=Book::findOrFail($this->bookId);
     $book->delete();
     $this->books=Book::all();
     $this->isModalOpen = false;
@@ -36,7 +36,7 @@ function cancel(){
 
 <div class="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-3 my-4 ">
      @foreach ($books as $book)
-         <div class="flex w-full bg-gray-500 px-4 py-5 flex-col gap-3 rounded-md text-gray-200">
+         <div class="flex w-full bg-gray-600 px-4 py-5 flex-col gap-3 rounded-md text-gray-200">
             <div class=" text-2xl font-bold ">
             <i class="fa-solid fa-book-open text-text-color "></i>
             </div>
@@ -44,17 +44,17 @@ function cancel(){
             <h1>Author:{{$book->author}}</h1>
             <div class="flex justify-between items-center">
             <span>Rating:{{$book->rating}}</span>
-            <button wire:click="deleteBook({{ $book->id }})" class="bg-red-700 text-gray-200 rounded-md py-2 px-4 ">Delete</button>
+            <button wire:click="deleteBook( {{ $book->id }})" class="bg-red-700 text-gray-200 rounded-md py-2 px-4 ">Delete</button>
             </div>
          </div>
      @endforeach
      @if ($isModalOpen)
     <div class="absolute top-1/2 left-1/2 bg-black/75 -translate-1/2 rounded-md">
     <div class="fles flex-col text-white p-5 rounded-md ">
-        <span class="font-bold ">Delete a book</span>
+        {{-- <span class="font-bold ">Delete a book</span> --}}
         <h1 class="font-bold ">Are you sure you want to delete this book?</h1>
     </div>
-    <div class="flex my-4 justify-between p-2 items-center">
+    <div class="flex my-4 justify-between p-4 px-6 items-center">
        <button wire:click="cancel" class="bg-white px-3 py-2   rounded-md">Cancel</button>
        <button wire:click="deleteBtn" class="bg-red-700 px-3 py-2 rounded-md">Delete</button>
     </div>
