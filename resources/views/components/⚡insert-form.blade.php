d23bb03f02dc3872279c9b2d5bfe9c3bc9287673
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-28 21:55:18 +0430

    addBookForm

--- resources/views/components/⚡insert-form.blade.php
+++ resources/views/components/⚡insert-form.blade.php
@@ -12,7 +12,12 @@ new class extends Component
 };
 ?>
 
-<div>
-    
+<div class="border p-5 ">
+    @foreach ($book as $book )
+    <input wire:input.getTitle{$this.event.value} class="border" type="text"   placeholder="Please Enter book name">
+    <input wire:input.getAuthor{$this.event.value} class="border" type="text"   placeholder="Please Enter book author">
+    <input wire:input.getRating{$this.evnet.value} class="border" type="number" placeholder="Please Enter book rating">
+    <input class="w-1/2 pt-3 bg-orange-400 text-gray-200" type="submit">
+     @endforeach
     {{-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh --}}
 </div>
