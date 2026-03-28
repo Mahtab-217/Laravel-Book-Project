714b38a4d843f17cf3b8665c89ef67c1827e9b12
mahtab-217 <mahtabnasiri120089@gmail.com>
2026-03-28 21:48:27 +0430

    new

---
+++ resources/views/components/⚡insert-form.blade.php
@@ -0,0 +1,18 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+    public getBookProperty(){
+        return Book::all();
+    }
+
+};
+?>
+
+<div>
+    
+    {{-- The best way to take care of the future is to take care of the present moment. - Thich Nhat Hanh --}}
+</div>
